b1ae36efb73c04621650aded67377f1abe1cc915
sonnv102 <sonnvceo@gmail.com>
2014-12-03 09:59:17 +0700

    #Clubbers: Create project

---
+++ Clubbers/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Clubbers
+//
+//  Created by SonNV on 12/3/14.
+//  Copyright (c) 2014 Clubbers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Clubbers/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Clubbers
+//
+//  Created by SonNV on 12/3/14.
+//  Copyright (c) 2014 Clubbers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
